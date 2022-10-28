65019d328cc6bd4831eb19ee743fff348882a6ad
eug <yyh3531@163.com>
2022-10-28 20:13:04 +0800

    feat(e-tools): test

--- packages/website/src/App.vue
+++ packages/website/src/App.vue
@@ -2,8 +2,8 @@
  * @Author       : eug yyh3531@163.com
  * @Date         : 2022-10-28 15:44:07
  * @LastEditors  : eug yyh3531@163.com
- * @LastEditTime : 2022-10-28 15:47:54
- * @FilePath     : /rollup-ui-storeroom/packages/website/src/App.vue
+ * @LastEditTime : 2022-10-28 20:10:08
+ * @FilePath     : /ui-storeroom/packages/website/src/App.vue
  * @Description  : filename 
  * 
  * Copyright (c) 2022 by eug yyh3531@163.com, All Rights Reserved. 
@@ -11,16 +11,26 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { CalculateFontSize } from "e-tools";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  CalculateFontSize({ vw: 1440, size: 20 });
+});
 </script>
 
 <template>
   <div class="website-container">
     <h2>UI</h2>
     <Button msg="WTF"></Button>
+    <div class="title">WWW</div>
   </div>
 </template>
 
 <style scoped>
+.title {
+  font-size: 2rem;
+}
 .logo {
   height: 6em;
   padding: 1.5em;
--- packages/website/src/main.ts
+++ packages/website/src/main.ts
@@ -2,8 +2,8 @@
  * @Author       : eug yyh3531@163.com
  * @Date         : 2022-10-28 15:44:07
  * @LastEditors  : eug yyh3531@163.com
- * @LastEditTime : 2022-10-28 15:47:20
- * @FilePath     : /rollup-ui-storeroom/packages/website/src/main.ts
+ * @LastEditTime : 2022-10-28 20:09:11
+ * @FilePath     : /ui-storeroom/packages/website/src/main.ts
  * @Description  : filename
  * 
  * Copyright (c) 2022 by eug yyh3531@163.com, All Rights Reserved. 
@@ -12,7 +12,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import UI from 'ui'
-import 'ui/lib/theme-chalk/index.css'
+// import 'ui/lib/theme-chalk/index.css'
 const app = createApp(App)
 app.use(UI)
 app.mount('#app')
